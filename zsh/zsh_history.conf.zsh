e8ee7302c5d91fab301d94cfa7d7517f5e0a9083
tuzi3040 <28290621+tuzi3040@users.noreply.github.com>
2021-04-07 16:55:09 +0800

    feat(zsh_history_conf): complete full history option

--- zsh/zsh_history.conf.zsh
+++ zsh/zsh_history.conf.zsh
@@ -1,16 +1,24 @@
 # HISTFILE="$HOME/.zsh_history"
 HISTSIZE=2000000
 SAVEHIST=1000000
+# setopt APPEND_HISTORY
 # setopt BANG_HIST                 # Treat the '!' character specially during expansion.
-# setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
-# setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
-# setopt SHARE_HISTORY             # Share history between all sessions.
-setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
-setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
-# setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
-# setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
-setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
-# setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
-setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
-setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
+setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
+# setopt HIST_ALLOW_CLOBBER
 # setopt HIST_BEEP                 # Beep when accessing nonexistent history.
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
+# setopt HIST_FCNTL_LOCK
+# setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
+# setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
+setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
+setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
+# setopt HIST_LEX_WORDS
+# setopt HIST_NO_FUNCTIONS
+# setopt HIST_NO_STORE
+setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
+# setopt HIST_SAVE_BY_COPY
+# setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
+setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
+# setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+# setopt INC_APPEND_HISTORY_TIME
+setopt SHARE_HISTORY             # Share history between all sessions.
